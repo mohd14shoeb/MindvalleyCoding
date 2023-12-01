b0b3d885495d7bfc854cbf783ea124fffc7e1a67
Shoeb Khan <>
2023-12-01 15:03:33 +0530

    Project initiated code

---
+++ MindvalleyCoding/MindvalleyCoding/MindvalleyCodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  MindvalleyCodingApp.swift
+//  MindvalleyCoding
+//
+//  Created by Shoeb Khan on 01/12/23.
+//
+
+import SwiftUI
+
+@main
+struct MindvalleyCodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
